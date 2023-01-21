9f706c786e08ef70f8e9b69f780d23c4b9baef62
kringier <kevinringier@gmail.com>
2023-01-21 14:46:47 -0700

    using immutable list in rook

--- src/main/scala/piece/Rook.scala
+++ src/main/scala/piece/Rook.scala
@@ -6,17 +6,10 @@ case class Rook() extends Piece:
 
 given rookMoves: Moveable[Rook] with
   def moves(pos: Position): List[Position] =
-    // does this need to be mutable, is that the bug?
-    // if so, we have 2 options, make it mutable, or refactor using immutable lists
-    val moves: scala.collection.mutable.List[Position] = List()
+    val filePositions = for i <- 1 to 8 if i != pos.file
+    yield Position(pos.rank, i)
 
-    for i <- 1 to 8 if i != pos.file
-      do //Position(pos.rank, i) :: moves
-        val newPos = Position(pos.rank, i)
-        newPos :: moves
+    val rankPositions = for i <- 1 to 8 if i != pos.rank
+    yield Position(i, pos.file)
 
-    for i <- 1 until 8
-      do if (i != pos.rank) then Position(i, pos.file) :: moves
-
-    //moves
-    List(Position(1,2))
+    (rankPositions ++ filePositions).toList
